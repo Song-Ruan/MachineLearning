32e9b9ce9729b373ed3e4ce699d178b5ed487470
Dream <1104122155@qq.com>
2018-08-24 15:28:34 +0800

    推荐系统修改

--- ex8/RecommenderSystem.py
+++ ex8/RecommenderSystem.py
@@ -59,8 +59,8 @@ print(computeCost(serialize(X,Theta),Y,R,nm,nu,nf),computeCost(serialize(X,Theta
 def computeGrad(params,Y,R,nm,nu,nf,l=0):
 
     X, Theta = deserialize(params, nm, nu, nf)
-    dx=((X.dot(Theta.T)-Y)*R).dot(Theta)+1*X#记住求grad的时候千万不要sum，因为此时期待得出的是nm*nf的矩阵而不是一个值
-    dtheta=((X.dot(Theta.T)-Y)*R).T.dot(X)+1*Theta
+    dx=((X.dot(Theta.T)-Y)*R).dot(Theta)+l*X#记住求grad的时候千万不要sum，因为此时期待得出的是nm*nf的矩阵而不是一个值
+    dtheta=((X.dot(Theta.T)-Y)*R).T.dot(X)+l*Theta
     return serialize(dx,dtheta)
 
 #数值校验
@@ -69,16 +69,16 @@ def checkGradient(params,Y,myR,nm,nu,nf,l=0):
     grad=computeGrad(params,Y,myR,nm,nu,nf,l)
 
     e=0.0001
-    nparams=len(params)
-    e_vec=np.zeros(nparams)
+    nparams=len(params)#有nparams个梯度需要计算
+    e_vec=np.zeros(nparams)#用作delt存放之处
 
     for i in range(10):
         idx=np.random.randint(0,nparams)
-        e_vec[idx]=e
+        e_vec[idx]=e#只有要求偏导的某个变量的delt需要有变化
         loss1=computeCost(params-e_vec,Y,myR,nm,nu,nf,l)
         loss2=computeCost(params+e_vec,Y,myR,nm,nu,nf,l)
         numgrad=(loss2-loss1)/(2*e)
-        e_vec[idx]=0
+        e_vec[idx]=0#需同时求梯度，因此要改回来
         diff=np.linalg.norm(numgrad-grad[idx])/np.linalg.norm(numgrad+grad[idx])
         print('%0.15f \t %0.15f \t %0.15f' %(numgrad, grad[idx], diff))
 print("Checking gradient with lambda = 0...")
@@ -110,8 +110,9 @@ for i in range(len(my_ratings)):
     if my_ratings[i]>0:
         print(my_ratings[i],movies[i])
 
-mat = io.loadmat('data/ex8_movies.mat')
+mat = io.loadmat('ex8_movies.mat')
 Y, R = mat['Y'], mat['R']
+
 #把自己的爱好和数据合并
 Y=np.c_[Y,my_ratings]
 R=np.c_[R,my_ratings!=0]
@@ -121,11 +122,16 @@ nf=10
 
 #均值归一化
 def featureNormalize(Y,R):
-    Ymean=(Y.sum(axis=1)/R.sum(axis=1)).reshape(-1,1)
+    Ymean=((Y*R).sum(axis=1)/R.sum(axis=1)).reshape(-1,1)
     Ynorm=(Y-Ymean)*R#没有评分的不要计算在内，所以要elementwiseR
     return Ynorm,Ymean
 
 Ynorm,Ymean=featureNormalize(Y,R)
+
+'''
+有一个问题是不知道为什么最后输出的评分居然不在0-5范围之内
+排除了featureNormalize的错误
+'''
 #随机生成参数X,Theta矩阵
 X=np.random.random((nm,nf))
 Theta=np.random.random((nu,nf))
@@ -133,20 +139,20 @@ params=serialize(X,Theta)
 l=10
 
 
-res=opt.minimize(fun=computeCost,x0=params,args=(Y,R,nm,nu,nf,l),method='TNC',jac=computeGrad,options={'maxiter':100})
-ret=res.x
+res=opt.minimize(fun=computeCost,x0=params,args=(Ynorm,R,nm,nu,nf,l),method='TNC',jac=computeGrad,options={'maxiter':100})
 
-fit_X, fit_Theta = deserialize(ret, nm, nu, nf)
+
+fit_X, fit_Theta = deserialize(res.x, nm, nu, nf)
 
 
 #用训练好的参数预测用户未评价电影的分数
 pred_mat=fit_X.dot(fit_Theta.T)
-
 #之前添加的用户的预测分
 pred=pred_mat[:,-1]+Ymean.flatten()
+#[:,-1]取所有行的最后一个数据组成
 
-print(pred)
-pred_sorted_idx=np.argsort(pred)[::-1]
+#将预测评分排序并从大到小排列
+pred_sorted_idx=np.argsort(pred)[::-1]#[::-1]表示反转倒序
 
 print("Top recommendations for you:")
 for i in range(10):
